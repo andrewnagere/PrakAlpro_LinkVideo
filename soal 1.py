852a2ca2986d9138066132ae036cdc46902f5669
andrewnagere <78341350+andrewnagere@users.noreply.github.com>
2021-02-24 00:19:11 +0800

    Create soal 1.py

---
+++ soal 1.py
@@ -0,0 +1,20 @@
+harga_dulu = 650000
+harga_sekarang = 685000
+emas_yang_dibeli = 25
+untung = (harga_sekarang - harga_dulu) * 25
+print (untung)
+persen = untung / (harga_dulu * 25) * 100
+print (persen)
+
+print ("========================")
+
+harga_dulu1 = 685000 * 15
+harga_dulu2 = 715000 * 15
+untung2 = harga_dulu2  - harga_dulu1 
+print (untung2)
+persen1 = untung2 / (harga_dulu1) * 100
+print (persen1)
+
+print ("========================")
+
+
